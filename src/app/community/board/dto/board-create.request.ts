c194b3558328d9bfe42f25713cd68853b451271f
puleugo <puleugo@gmail.com>
2023-03-27 19:47:45 +0900

    Feat: DTO 구조 변경

--- src/app/community/board/dto/board-create.request.ts
+++ src/app/community/board/dto/board-create.request.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import { BoardCreateRequestCommand } from '@app/community/board/board.commands';
 
 export class BoardCreateRequest implements BoardCreateRequestCommand {
-  title: string;
+  @ApiProperty({ example: '주제1', description: '게시판 주제' })
+  topic: string;
+
+  @ApiProperty({ example: '게시판 설명', nullable: true })
   description: string | null;
 }
